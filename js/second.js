99bb79209c8b6fd657720c48794d9534d73c52c6
zingrin <jerinloncheu@gmail.com>
2025-02-22 20:20:33 +0600

    summary

---
+++ js/second.js
@@ -0,0 +1,6 @@
+console.log('second file');
+const secondList = document.getElementById('second-list');
+// 
+const li = document.createElement('li');
+li.innerText = 'My dynamic item'
+secondList.appendChild(li);
